fab93e7f560ada7628e8a72a1b3377dd97e94431
Akash Kumar <48791126+akash1k997@users.noreply.github.com>
2020-07-11 11:15:56 +0530

    js dom

---
+++ index.js
@@ -0,0 +1,22 @@
+var randomNumber1 = Math.floor(Math.random()*6) +1;
+
+var imgSource1 = "images/dice"+randomNumber1+".png";
+
+document.querySelectorAll("img")[0].setAttribute("src",imgSource1);
+
+
+var randomNumber2 = Math.floor(Math.random()*6) +1;
+
+var imgSource2 = "images/dice"+randomNumber2+".png";
+
+document.querySelectorAll("img")[1].setAttribute("src",imgSource2);
+
+if(randomNumber1>randomNumber2){
+  document.querySelector("h1").innerHTML="🚩Player 1 wins";
+}
+else if( randomNumber2>randomNumber1){
+  document.querySelector("h1").innerHTML="Player 2 wins🚩";
+}
+else{
+  document.querySelector("h1").innerHTML="Draw";
+}
